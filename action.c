8befebaee4d61803649e96c81f5ec730c80ac269
fanilran <fanilran@student.42antananarivo.mg>
2026-09-18 14:48:17 +0300

    corrige: action.c && simulation.c

--- action.c
+++ action.c
@@ -6,65 +6,72 @@
 /*   By: fanilran <fanilran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/26 15:01:17 by fanilran          #+#    #+#             */
-/*   Updated: 2026/09/17 14:15:04 by fanilran         ###   ########.fr       */
+/*   Updated: 2026/09/18 14:45:36 by fanilran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
+static int	is_stopped(t_coder *coder)
+{
+	int	stopped;
+
+	pthread_mutex_lock(&coder->config->stop_mutex);
+	stopped = coder->config->stop;
+	pthread_mutex_unlock(&coder->config->stop_mutex);
+	return (stopped);
+}
+
+
+static void	wait_ms(t_coder *coder, long time)
+{
+	long	start;
+
+	start = get_current_ms();
+	while (get_current_ms() - start < time)
+	{
+		if (is_stopped(coder))
+			return ;
+		usleep(200);
+	}
+}
+
+
 void	compiles(t_coder *coder)
 {
 	long	timestamp;
-	long	start;
-	int		stoped;
-	
-	pthread_mutex_lock(&coder->config->stop_mutex);
-	stoped = coder->config->stop;
-	pthread_mutex_unlock(&coder->config->stop_mutex);
-	if (stoped)
+
+	if (is_stopped(coder))
 		return ;
 	pthread_mutex_lock(&coder->config->print_mutex);
 	timestamp = get_timestamp_ms(coder->config->start_time);
 	printf("%ld %d is compiling\n", timestamp, coder->id);
 	pthread_mutex_unlock(&coder->config->print_mutex);
-	start = get_current_ms();
-	while (!coder->config->stop && get_current_ms() - start <= coder->config->time_to_compile)
-	{
-		usleep(1000);
-	}
-	// usleep(coder->config->time_to_compile * 1000);
+	wait_ms(coder, coder->config->time_to_compile);
 }
 
 void	debuges(t_coder *coder)
 {
 	long	timestamp;
-	long		start;
 
+	if (is_stopped(coder))
+		return ;
 	pthread_mutex_lock(&coder->config->print_mutex);
 	timestamp = get_timestamp_ms(coder->config->start_time);
 	printf("%ld %d is debugging\n", timestamp, coder->id);
 	pthread_mutex_unlock(&coder->config->print_mutex);
-	start = get_current_ms();
-	while (!coder->config->stop && get_current_ms() - start <= coder->config->time_to_compile)
-	{
-		usleep(1000);
-	}
-	// usleep(coder->config->time_to_debug * 1000);
+	wait_ms(coder, coder->config->time_to_compile);
 }
 
 void	refactores(t_coder *coder)
 {
 	long	timestamp;
-	long		start;
 
+	if (is_stopped(coder))
+		return ;
 	pthread_mutex_lock(&coder->config->print_mutex);
 	timestamp = get_timestamp_ms(coder->config->start_time);
 	printf("%ld %d is refactoring\n", timestamp, coder->id);
 	pthread_mutex_unlock(&coder->config->print_mutex);
-	start = get_current_ms();
-	while (!coder->config->stop && get_current_ms() - start <= coder->config->time_to_compile)
-	{
-		usleep(1000);
-	}
-	// usleep(coder->config->time_to_refactor * 1000);
+	wait_ms(coder, coder->config->time_to_compile);
 }
--- simulation.c
+++ simulation.c
@@ -6,7 +6,7 @@
 /*   By: fanilran <fanilran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/11 11:14:21 by fanilran          #+#    #+#             */
-/*   Updated: 2026/09/17 14:13:54 by fanilran         ###   ########.fr       */
+/*   Updated: 2026/09/18 14:45:49 by fanilran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,42 +61,29 @@ int	check_all_done(t_coder *coders)
 void	*routine(void *arg)
 {
 	t_coder	*coder;
-	int		stoped;
 	int		i;
 
 	coder = (t_coder *)arg;
 	i = 0;
 	while (i < coder->config->number_of_compiles_required)
 	{
-		pthread_mutex_lock(&coder->config->stop_mutex);
-		stoped = coder->config->stop;
-		pthread_mutex_unlock(&coder->config->stop_mutex);
-		if (stoped)
-			break ;
 		take_dongle(coder);
+
 		pthread_mutex_lock(&coder->activity_mutex);
 		coder->last_compile_start = get_timestamp_ms(coder->config->start_time);
 		pthread_mutex_unlock(&coder->activity_mutex);
+
 		compiles(coder);
+
 		release_dongle(coder, coder->left);
 		release_dongle(coder, coder->right);
-	
-		pthread_mutex_lock(&coder->config->stop_mutex);
-		stoped = coder->config->stop;
-		pthread_mutex_unlock(&coder->config->stop_mutex);
-		if (stoped)
-			break ;
 
 		debuges(coder);
-
-		pthread_mutex_lock(&coder->config->stop_mutex);
-		stoped = coder->config->stop;
-		pthread_mutex_unlock(&coder->config->stop_mutex);
-		if (stoped)
-			break ;
 		
 		refactores(coder);
+
 		i++;
+
 		pthread_mutex_lock(&coder->activity_mutex);
 		coder->compile_done = i;
 		pthread_mutex_unlock(&coder->activity_mutex);
